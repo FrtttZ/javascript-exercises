1956ef5bed3e93d7918728648dd568eb33d6f8b6
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 15:42:12 +0800

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
 const findTheOldest = function(people) {
     let currentYear = new Date().getFullYear();
-    return currentYear;
+
+    let oldest = people.reduce((oldestSoFar, currentPerson) => {
+
+    
+        if (oldestSoFar.yearOfDeath == undefined || 
+            currentPerson.yearOfDeath == undefined){
+            oldestSoFar.yearOfDeath = currentYear;
+        }
+
+        
+       if(oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth >
+        currentPerson.yearOfDeath - currentPerson.yearOfBirth
+       ){
+        return oldestSoFar;
+       }else{
+        return currentPerson;
+       }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
