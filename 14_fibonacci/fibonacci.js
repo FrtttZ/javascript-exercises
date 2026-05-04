840ace0578c5ee23dc859548a0055b754a6f528f
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 14:53:54 +0800

    Accepts string and validates negative number

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,11 +1,17 @@
 const fibonacci = function(n) {
     let prevNum = [1,1]
-
-    if(n <= 0){
+    n = parseInt(n);
+    if(n === 0){
         return 0;
-    }else if(n === 1 || n === 2) return 1;
+    }else if(n === 1 || n === 2){
+        return 1;
+    }else if(n < 0){
+        return 'OOPS';
+    }
 
-     let nthValue = 0;
+
+
+    let nthValue = 0;
     for(let i = 2; i < n; i++){
         nthValue =  prevNum[0] + prevNum[1] // 2
         //set a to 2
