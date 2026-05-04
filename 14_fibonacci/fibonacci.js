5943abb8e431d2d547f1e9536242f427facd3d06
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 14:47:50 +0800

    validates low number input

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let prevNum = [1,1]
+
+    if(n <= 0){
+        return 0;
+    }else if(n === 1 || n === 2) return 1;
+
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
