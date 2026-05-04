276bead1cb7f7b1f64d35f365022e3b0a481ee5b
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 14:48:16 +0800

    Logic completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -5,7 +5,17 @@ const fibonacci = function(n) {
         return 0;
     }else if(n === 1 || n === 2) return 1;
 
+     let nthValue = 0;
+    for(let i = 2; i < n; i++){
+        nthValue =  prevNum[0] + prevNum[1] // 2
+        //set a to 2
 
+        prevNum.shift();
+        prevNum.push(nthValue);
+
+        
+    }
+    return nthValue;
 };
 
 // Do not edit below this line
