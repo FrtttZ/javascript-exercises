aab589aa7c4c6ec30a8569f953b4d2c89bd970c8
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 15:03:23 +0800

    Get current year

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    return currentYear;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
